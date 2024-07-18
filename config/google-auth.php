91e25db694db4912c69fe7197707db40c3477ed1
David Rushton <david@papertank.co.uk>
2024-07-18 13:23:55 +0100

    Updated env to GOOGLE_AUTH_SERVICE_CREDENTIALS

--- config/google-auth.php
+++ config/google-auth.php
@@ -6,7 +6,7 @@ return [
     'default' => 'service_account',
 
     'service_account' => [
-        'credentials' => env('GOOGLE_APPLICATION_CREDENTIALS', storage_path('app/google-auth/service-account-credentials.json')),
+        'credentials' => env('GOOGLE_AUTH_SERVICE_CREDENTIALS', storage_path('app/google-auth/service-account-credentials.json')),
     ],
 
     'cache' => [
